b2aaa45bd8f690ad1a87cece9560dcae06bc2538
AngusJK <anguskellett@hotmail.com>
2015-03-13 18:31:31 +0000

    commit1

--- contact.rb
+++ contact.rb
@@ -1,19 +1,29 @@
- class Contact
+require_relative 'contacts.csv'
+
+class Contact
  
   attr_accessor :name, :email
  
   def initialize(name, email)
-    # TODO: assign local variables to instance variables
+    @name = name
+    @email = email
   end
  
   def to_s
     # TODO: return string representation of Contact
+    self.to_s
   end
  
   ## Class Methods
   class << self
+
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
+      contact = [name, email]
+      
+      CSV.open('contacts.csv', 'w') do |csv_object|
+        csv_object << contact
+      end
     end
  
     def find(index)
