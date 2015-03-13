4666218aee6455ce7e4a5d44297aeb922cd279ab
AngusJK <anguskellett@hotmail.com>
2015-03-13 18:33:12 +0000

    updated

--- contact_list.rb
+++ contact_list.rb
@@ -1,2 +1,34 @@
 require_relative 'contact'
 require_relative 'contact_database'
+require 'csv'
+
+def run
+
+  userprompt = ARGV.first
+
+  if userprompt == "help"
+    puts "Here is a list of available commands:
+    new - create a new contact
+    list - list all contacts
+    show - show a contact
+    find - find a contact"
+  elsif userprompt == "new"
+    puts "Please enter new contact name:"
+    name = $stdin.gets.chomp
+    puts "Please enter new contact e-mail:"
+    email = $stdin.gets.chomp
+    Contact.create(name, email)
+  elsif userprompt == "list"
+    
+  elsif userprompt == "show"
+
+  elsif userprompt == "find"
+
+  else
+    puts "That is not a valid command."  
+  end
+
+end
+
+run
+
